8b400018ff091c9c3a6c83a814a15785d63d1a0e
LeToyek <maulanatoyek@gmail.com>
2022-07-12 22:17:31 +0700

    send message done

--- src/components/ChatField.jsx
+++ src/components/ChatField.jsx
@@ -1,35 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
+import { useAppContext } from "../context/AppContext";
 
 const ChatField = () => {
+  const {sendMessage} = useAppContext()
+  const [text,setText] = useState("")
   return (
     <div className="chat-field">
       <div className="chat-container">
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
+        <ChatCard isUser={true} />
+        <ChatCard isUser={true} />
+        <ChatCard isUser={false} />
+        <ChatCard isUser={false} />
+        <ChatCard isUser={true} />
       </div>
       <div className="group-form">
-        <input type="text" />
-        <button>
+        <input type="text" value={text} onChange={e => setText(e.target.value)}/>
+        <button onClick={()=> sendMessage(text)}>
           <SendIcon />
         </button>
       </div>
     </div>
   );
 };
-const ChatCard = () => {
+const ChatCard = ({ isUser }) => {
   return (
     <>
-      <h5 className="chat-user">toyek 🥶<span>Today 6:34</span></h5>
-      <div className="chat-card">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam nesciunt, deserunt consequatur provident, nihil similique ipsa necessitatibus cum natus non alias quae modi, voluptatibus quod dignissimos voluptates architecto libero totam!
-        </p>
-      </div>
+      {isUser ? (
+        <div className="group-card user">
+          <h5 className="chat-user">
+            toyek 🥶<span>Today 6:34</span>
+          </h5>
+          <div className="chat-card">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
+              nesciunt, deserunt consequatur provident, nihil similique ipsa
+              necessitatibus cum natus non alias quae modi, voluptatibus quod
+              dignissimos voluptates architecto libero totam!
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="group-card">
+          <h5 className="chat-user">
+            toyek 🥶<span>Today 6:34</span>
+          </h5>
+          <div className="chat-card">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
+              nesciunt, deserunt consequatur provident, nihil similique ipsa
+              necessitatibus cum natus non alias quae modi, voluptatibus quod
+              dignissimos voluptates architecto libero totam!
+            </p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
-
 export default ChatField;
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
-import { createContext, useContext, useState } from "react";
-import { useHistory, useNavigate } from 'react-router-dom';
-
+import { createContext, useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const AppContext = createContext({});
 
 const AppContextProvider = ({ children }) => {
   const [Username, setUsername] = useState("");
   const [Password, setPassword] = useState("");
-  const [isWrong, setIsWrong] = useState(false)
-  const [isLoading,setIsLoading] = useState(false)
-  const history = useHistory()
+  const [userID,setUserID] = useState(null)
+  const [isWrong, setIsWrong] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [dataMessages, setDataMessages] = useState(null);
+  const history = useHistory();
   const supabase = createClient(
     "https://utybkjndivaewaatsisa.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0eWJram5kaXZhZXdhYXRzaXNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc1MTE2NjcsImV4cCI6MTk3MzA4NzY2N30.rx3pZlxCK8p1WQTy-zUiMNsUdKzVHwhltVRhzni38NE"
@@ -23,18 +24,44 @@ const AppContextProvider = ({ children }) => {
     try {
       let data = await supabase
         .from("users")
-        .select(`Password`)
+        .select(`id,Password`)
         .eq("Username", Username);
+      await setUserID(data.body[0].id)  
       if (data.body[0].Password === Password) {
-        setIsWrong(false)
+        setIsWrong(false);
         console.log("login success");
-        history.push("/chat")
-      }else{
-        setIsWrong(true)
+        history.push("/chat");
+      } else {
+        setIsWrong(true);
       }
-    } catch (error) {}
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  const getMessages = async () => {
+    try {
+      let data = await supabase
+        .from("messages")
+        .select(`created_at,time,users(Username)`);
+      setDataMessages(data);
+    } catch (error) {
+      throw new Error(error);
+    }
   };
 
+  const sendMessage = async (message) => {
+    try {
+      await supabase
+        .from("messages")
+        .insert([{ text: message, user_id: userID }])
+        .single();
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  useEffect(()=>{
+    console.log("matamu");
+  })
   const onHandleLogin = (e) => {
     e.preventDefault();
     getUserAccount();
@@ -53,7 +80,9 @@ const AppContextProvider = ({ children }) => {
         setPassword,
         onHandleLogin,
         onHandleRegister,
-        isWrong
+        isWrong,
+        sendMessage,
+        dataMessages
       }}
     >
       {children}
